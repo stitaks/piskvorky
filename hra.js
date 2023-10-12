1ac924ff39a831b014daa1df2293fc970cbb6dc6
stitaks <sarka.stitak@gmail.com>
2023-10-12 09:47:58 +0200

    Dokonceni 4.casti ukolu'

--- hra.js
+++ hra.js
@@ -1,4 +1,22 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
+
+const whoIsWinner = () => {
+    const poleWinner =[];
+    document.body.querySelectorAll('.game__box').forEach((button) => {
+        if (button.classList.contains('board__field--cross')){
+            poleWinner.push('x');
+        }
+        else if(button.classList.contains('board__field--circle')){
+            poleWinner.push('o');
+        }
+        else{
+            poleWinner.push('_');
+        }
+    })
+    return findWinner(poleWinner);
+}
 
 const change = (event) => {
     if (currentPlayer === 'cross') {
@@ -6,12 +24,25 @@ const change = (event) => {
         currentPlayer = 'circle';
         event.target.disabled = true;
         document.body.querySelector('.game__image').src = 'img/circle.svg';
+        
     }
     else {
         event.target.className += ' board__field--circle';
         currentPlayer = 'cross';
         event.target.disabled = true;
         document.body.querySelector('.game__image').src = 'img/cross.svg';
+    }
+const vitez = whoIsWinner();
+    if (vitez === 'o' || vitez === 'x') {
+        const alertOX = () => {alert(`Vyhrál hráč se symbolem ${vitez}.`);
+        location.reload();}
+        setTimeout(alertOX, 250);
+    }
+    else if (vitez === 'tie'){
+        const alertTIE = () => {alert('Hra skončila remízou.');
+        location.reload();
+        }
+        setTimeout(alertTIE, 250);
     }
 }
 
@@ -32,13 +63,7 @@ const home = (event) =>  {
 document.body.querySelector('.card_button--green').addEventListener('click', home);
 document.body.querySelector('.card_button--blue').addEventListener('click', restart);
 
-document.body.querySelector('button:nth-child(1)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(2)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(3)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(4)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(5)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(6)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(7)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(8)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(9)').addEventListener('click', change);
-document.body.querySelector('button:nth-child(10)').addEventListener('click', change);
+const buttons = document.body.querySelectorAll('.game__box');
+buttons.forEach((button) => {
+    button.addEventListener('click', change);
+});
